ad64ce0cce415c844237bdd3929e19c0fd32bf70
chintan-wadhwa <165909865+chintan-wadhwa@users.noreply.github.com>
2024-06-11 14:20:07 +0000

    Setting up new project

--- cool_project_name/settings.py
+++ cool_project_name/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app_name"
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
